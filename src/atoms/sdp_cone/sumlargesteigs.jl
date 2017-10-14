f14e16894ca73d642ebec9ca0a400e8477895414
Hua Zhou <huazhou@ucla.edu>
2017-10-13 17:05:36 -0700

    arguments to sumlargesteig need not be PSD

--- src/atoms/sdp_cone/sumlargesteigs.jl
+++ src/atoms/sdp_cone/sumlargesteigs.jl
@@ -61,9 +61,9 @@ function conic_form!(x::SumLargestEigs, unique_conic_forms)
     m, n = size(A)
     Z = Variable(n, n)
     s = Variable()
-    p = minimize(s*k + trace(Z), 
-                 Z + s*eye(n) - A ⪰ 0, 
-                 A ⪰ 0, Z ⪰ 0)
+    p = minimize(s*k + trace(Z),
+                 Z + s*eye(n) - A ⪰ 0,
+                 Z ⪰ 0)
     cache_conic_form!(unique_conic_forms, x, p)
   end
   return get_conic_form(unique_conic_forms, x)
